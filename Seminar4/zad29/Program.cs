ec5efc9a0a245febe2775abc47706c2ff12a9a59
levchakovd <levchakovd@yandex.ru>
2022-08-04 00:43:11 +0300

    zad28

---
+++ Seminar4/zad29/Program.cs
@@ -0,0 +1,8 @@
+﻿int [] array = new int [8];
+Console.WriteLine("Сейчас Вы введете 8 чисел, а я покажу их массивом");
+for (int i = 0; i< 8; i++)
+{
+    Console.WriteLine("Введите число с индексом "+ i);
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("["+ string.Join(", ", array)+ "]");
