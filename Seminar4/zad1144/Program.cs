b6fcd882d7075172fdecf3c9cb1c63e08fe3d528
levchakovd <levchakovd@yandex.ru>
2022-08-09 15:19:59 +0300

    ничего не вышло

---
+++ Seminar4/zad1144/Program.cs
@@ -0,0 +1,11 @@
+﻿int [] array = new int [100];
+Console.WriteLine("Введите массив");
+
+for (int i = 0; i<100 || array[i] !=0; i++)
+{
+    array[i]=Convert.ToInt32(Console.ReadLine());
+
+}
+
+// Ввод массива не обрывается после ввода 0
+// Не получается ввести все элементы массива, выдает ошибку
