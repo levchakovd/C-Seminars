6181b226f2c5a3fe9c3cd1e22feaa98b24516b5f
levchakovd <levchakovd@yandex.ru>
2022-08-11 17:00:22 +0300

    zad41

---
+++ Sem6/zad41/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Сколько чисел М, Вы введете?");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[M];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine("Введите число с индексом "+i);
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+int count= 0;
+for (int i = 0; i < array.Length; i++)
+if (array[i]>0)
+    count++;
+
+Console.WriteLine("Положительных чисел "+ count);
