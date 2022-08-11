3d6b14f5d737af4c1adfd6723d0f8e8efdc3aa6c
levchakovd <levchakovd@yandex.ru>
2022-08-11 16:26:46 +0300

    zad36

--- Seminar5/Zad36/Program.cs
+++ Seminar5/Zad36/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.WriteLine("Введите размер массива");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i<array.Length;i++)
+    array[i]=new Random().Next(0,100);
+
+Console.WriteLine("["+ string.Join(",",array)+"]");
+
+int summ = 0;
+for (int i = 1; i< array.Length;i=i+2)
+
+    summ = summ+array[i];
+
+Console.WriteLine(summ);
