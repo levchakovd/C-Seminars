e5577751065ace82da8576a77c688a15a2a24429
levchakovd <levchakovd@yandex.ru>
2022-08-16 17:50:55 +0300

    что-то не сраслось

---
+++ Sem6/zad1233/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите размер матрицы");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[N, N];
+for(int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array[i, j] = Convert.ToInt32(Console.ReadLine());
+}
+for(int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i, j] + " ");
+    Console.WriteLine("\t");
+}
+
