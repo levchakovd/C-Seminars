cfde58f397eeae69b19768b74161ac0cafbc551e
levchakovd <levchakovd@yandex.ru>
2022-08-16 22:01:11 +0300

    подглядел решение на идентичной задаче на семинаре

--- Sem6/zad1233/Program.cs
+++ Sem6/zad1233/Program.cs
@@ -1,16 +1,42 @@
-﻿Console.WriteLine("Введите размер матрицы");
-int N = Convert.ToInt32(Console.ReadLine());
+﻿Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
 
-int[,] array = new int[N, N];
-for(int i = 0; i < array.GetLength(0); i++)
+for (int i = 0; i < n; i++)
 {
-    for (int j = 0; j < array.GetLength(1); j++)
-        array[i, j] = Convert.ToInt32(Console.ReadLine());
+    for (int j = 0; j < m; j++)
+    {
+        matrix[i, j] = new Random().Next(0, 10);
+    }
 }
-for(int i = 0; i < array.GetLength(0); i++)
+for (int i = 0; i < n; i++)
 {
-    for (int j = 0; j < array.GetLength(1); j++)
-        Console.Write(array[i, j] + " ");
-    Console.WriteLine("\t");
+    for (int j = 0; j < m; j++)
+        Console.Write(matrix[i, j] + " \t");
+    Console.WriteLine();
 }
+Console.WriteLine("транспонируем по диагонали");
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        int temp = 0;
+        if (i<j)
+        {
+        temp = matrix[i,j];
+        matrix[i,j] = matrix[j,i];
+        matrix[j,i] = temp;
 
+        }
+
+    }
+}
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+        Console.Write(matrix[i, j] + " \t");
+    Console.WriteLine();
+}
