c34c3abb131d038ce6e169f6f1ae0dc19641b0da
levchakovd <levchakovd@yandex.ru>
2022-08-17 22:35:17 +0300

    zad50

---
+++ Sem7/zad50/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        matrix[i, j] = new Random().Next(0, 10);
+    }
+}
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+        Console.Write(matrix[i, j] + " \t");
+    Console.WriteLine();
+}
+
+Console.Write("Введите число, которое мы ищем: ");
+int k = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        
+        if (matrix[i,j]==k)
+            count++;  
+    }
+}
+if (count>0)
+    Console.WriteLine("Число "+ k + " встречается " + count + " раз");
+else
+{
+    Console.WriteLine("Такого числа нет");
+}
