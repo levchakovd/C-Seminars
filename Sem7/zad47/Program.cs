e226ab6c9783051f654c0b1fbac67f3212c00a02
levchakovd <levchakovd@yandex.ru>
2022-08-17 21:50:35 +0300

    zad47

---
+++ Sem7/zad47/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+double[,] matrix = new double[n, m];
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+        matrix[i, j] = Convert.ToDouble(new Random().NextDouble()*10);
+        
+    
+}
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+        Console.Write( Math.Round(matrix[i, j],1) + " \t");
+    Console.WriteLine();
+}
---
+++ Sem7/zad48/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 48: Задайте двумерный массив размера m на n, 
+//каждый элемент в массиве находится по формуле: Aₘₙ = m+n. 
+//Выведите полученный массив на экран.
+//m = 3, n = 4.
+//0 1 2 3
+//1 2 3 4
+//2 3 4 5
+int m = Convert.ToInt32(Console.ReadLine());;
+int n = Convert.ToInt32(Console.ReadLine());;
+int[,] array = new int[m, n];
+for(int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array[i, j] = i+j;
+}
+for(int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i, j] + " ");
+    Console.WriteLine("\t");
+}
