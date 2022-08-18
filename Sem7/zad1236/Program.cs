30b190598c36a2460883d7ac53dd9a1f87eef971
levchakovd <levchakovd@yandex.ru>
2022-08-18 03:45:17 +0300

    zad 1236

---
+++ Sem7/zad1236/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        matrix[i, j] = new Random().Next(0, 10);
+    }
+}
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+        Console.Write(matrix[i, j] + " \t");
+    Console.WriteLine();
+}
+Console.WriteLine("Транспонация");
+
+for (int i = 0; i < (n/2); i++)
+{
+    int temp = 0;
+    for (int j = 0; j < m; j++)
+    {
+        temp = matrix[i, j];
+        matrix[i,j] = matrix[n-1-i,j];
+        matrix[n-1-i,j]=temp;
+    }
+}
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+        Console.Write(matrix[i, j] + " \t");
+    Console.WriteLine();
+}
